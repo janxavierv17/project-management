9320b242a88206d9e72457cb7e5c543545166794
Jan Xavier Virgen <54022069+janxavierv17@users.noreply.github.com>
2024-09-10 21:14:06 +1000

    Sidebar & Navbar with actions

--- client/src/app/components/Navbar/index.tsx
+++ client/src/app/components/Navbar/index.tsx
@@ -1,12 +1,29 @@
-import { Search, Settings } from 'lucide-react';
+import { setIsDarkMode, setIsSidebarCollapsed } from '@/redux/state';
+import { useAppDispatch, useAppSelector } from '@/redux/store';
+import { Menu, Moon, Search, Settings, Sun } from 'lucide-react';
 import Link from 'next/link';
 
 export function Navbar() {
+  const dispatch = useAppDispatch();
+  const { isSidebarCollapsed, isDarkMode } = useAppSelector((state) => state.global);
+  const showButton = isSidebarCollapsed ? (
+    <button onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))}>
+      <Menu className="h-8 w-8 dark:text-white" />
+    </button>
+  ) : null;
+
+  const iconClassName = 'h-6 w-6 cursor-pointer';
+  const showIcon = isDarkMode ? (
+    <Sun className={iconClassName} />
+  ) : (
+    <Moon className={iconClassName} />
+  );
+
   return (
     <div className="flex items-center justify-between bg-white px-4 py-3 dark:bg-black">
       {/* Search bar */}
-
       <div className="flex items-center gap-8">
+        {showButton}
         <div className="relative flex h-min w-[200px]">
           <Search className="absolute left-[4px] top-1/2 mr-2 h-5 w-5 -translate-y-1/2 transform cursor-pointer dark:text-white" />
           <input
@@ -18,6 +35,12 @@ export function Navbar() {
       </div>
       {/* Icons */}
       <div className="flex items-center">
+        <button
+          onClick={() => dispatch(setIsDarkMode(!isDarkMode))}
+          className={`h-min w-min rounded p-2 ${isDarkMode ? `dark:hover:bg-gray-700` : `hover:bg-gray-100`}`}
+        >
+          {showIcon}
+        </button>
         <Link href="settings" className="h-min w-min rounded p-2 hover:bg-gray-100">
           <Settings className="h-6 w-6 cursor-pointer dark:text-white" />
         </Link>
--- client/src/app/components/Sidebar/index.tsx
+++ client/src/app/components/Sidebar/index.tsx
@@ -1,14 +1,74 @@
 'use client';
-import { LockIcon } from 'lucide-react';
-import Image from 'next/image';
 import { useState } from 'react';
+import {
+  LockIcon,
+  Icon,
+  LucideIcon,
+  Home,
+  X,
+  Briefcase,
+  Search,
+  Settings,
+  User,
+  Users,
+} from 'lucide-react';
+import Image from 'next/image';
+import { useAppSelector } from '@/redux/store';
+import { usePathname } from 'next/navigation';
+import { useAppDispatch } from '../../../redux/store';
+import Link from 'next/link';
+import { setIsSidebarCollapsed } from '@/redux/state';
+
+const navigationLinks = [
+  { icon: Home, label: 'Home', href: '/' },
+  { icon: Briefcase, label: 'Timeline', href: '/timeline' },
+  { icon: Search, label: 'Search', href: '/search' },
+  { icon: Settings, label: 'Settings', href: '/settings' },
+  { icon: User, label: 'User', href: '/users' },
+  { icon: Users, label: 'Team', href: '/team' },
+];
+interface ISidebarLink {
+  href: string;
+  icon: LucideIcon;
+  label: string;
+}
+
+function SidebarLink({ href, icon: Icon, label }: ISidebarLink) {
+  const pathname = usePathname();
+  const isActive = pathname === href || (pathname === '/' && href === '/dashboard');
+
+  return (
+    <Link href={href} className="w-full">
+      <div
+        className={`relative flex cursor-pointer items-center gap-3 transition-colors hover:bg-gray-100 dark:bg-black dark:hover:bg-gray-700 ${isActive ? 'bg-gray-100 text-white dark:bg-gray-600' : ''} justify-start px-8 py-3`}
+      >
+        {isActive && <div className="absolute left-0 top-0 h-[100%] w-[5px] bg-blue-200"></div>}
+        <Icon className="h-6 w-6 text-gray-800 dark:text-gray-100" />
+        <span className={`font-medium text-gray-800 dark:text-gray-100`}>{label}</span>
+      </div>
+    </Link>
+  );
+}
 
 export function Sidebar() {
   const [showProjects, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
 
+  const dispatch = useAppDispatch();
+  const { isSidebarCollapsed } = useAppSelector((state) => state.global);
+
+  const showButton = isSidebarCollapsed ? null : (
+    <button onClick={() => dispatch(setIsSidebarCollapsed(!isSidebarCollapsed))} className="py-3">
+      <X className="h-6 w-6 text-gray-600 hover:text-gray-500 dark:text-white" />
+    </button>
+  );
+  const links = navigationLinks.map(({ href, label, icon }) => (
+    <div key={label}>
+      <SidebarLink href={href} label={label} icon={icon} />
+    </div>
+  ));
   const sidebarClassNames = `fixed flex flex-col h-[100%] justify-between shadow-xl
-  transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white w-64
+  transition-all duration-300 h-full z-40 dark:bg-black overflow-y-auto bg-white ${isSidebarCollapsed ? 'w-0 hidden' : 'w-64'}
   `;
 
   return (
@@ -17,6 +77,7 @@ export function Sidebar() {
         {/* TOP LOGO */}
         <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
           <div className="text-xl font-bold text-gray-800 dark:text-white">JXV LIST</div>
+          {showButton}
         </div>
         {/* TEAM */}
         <div className="flex items-center gap-5 border-y-[1.5px] border-gray-200 px-8 py-4 dark:border-gray-700">
@@ -29,6 +90,8 @@ export function Sidebar() {
             </div>
           </div>
         </div>
+        {/* NAVBAR LINKS */}
+        <nav className="z-10 w-full">{links}</nav>
       </div>
     </div>
   );
